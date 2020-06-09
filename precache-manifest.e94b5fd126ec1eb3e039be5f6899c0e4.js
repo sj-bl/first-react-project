a0270aa4e75a09f4305e04d70dc0cb7d3d0467a1
sj-bl <sj.gamersnation@gmail.com>
2020-06-09 18:54:19 +0530

    Updates

---
+++ precache-manifest.e94b5fd126ec1eb3e039be5f6899c0e4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "af0f0cf4b01fad43b202c18795460f52",
+    "url": "/sj-bl/first-react-project/index.html"
+  },
+  {
+    "revision": "5440e3147517cf7b8962",
+    "url": "/sj-bl/first-react-project/static/css/main.29f08491.chunk.css"
+  },
+  {
+    "revision": "602be1621433c58a1245",
+    "url": "/sj-bl/first-react-project/static/js/2.edbc6255.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/sj-bl/first-react-project/static/js/2.edbc6255.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5440e3147517cf7b8962",
+    "url": "/sj-bl/first-react-project/static/js/main.d5b482f0.chunk.js"
+  },
+  {
+    "revision": "2d9dcdda270e129acd3d",
+    "url": "/sj-bl/first-react-project/static/js/runtime-main.ad36d125.js"
+  }
+]);
